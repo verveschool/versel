41f7fe59df2e335c7e697a237cbdc081ced571c2
Duggal <84291846+verveschool@users.noreply.github.com>
2026-05-16 11:18:25 +0530

    Remove copyright footer from partners page
    Removed footer section with copyright information.

--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -247,10 +247,6 @@ export default function PartnersPage() {
               </ul>
             </div>
           </section>
-
-          <div className="mt-20 pt-12 border-t border-border text-[13px]">
-            <p>© 2025 verveschool </p>
-          </div>
         </main>
 
         {/* Table of Contents */}
