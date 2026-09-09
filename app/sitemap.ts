6ecb0466c5863955646de3fcec096a214e5f9c84
A. Duggal <84291846+verveschool@users.noreply.github.com>
2026-09-10 01:10:36 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,7 +17,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: absoluteUrl("/about"),
+      url: absoluteUrl("/candidates"),
       changeFrequency: "monthly",
       priority: 0.8,
     },
